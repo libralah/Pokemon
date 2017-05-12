c280c985461c96b6209e51ff6c3a2dea447a7257
libralah <hungnguyen@Hungs-Mac-mini.fios-router.home>
2017-05-12 13:25:32 -0400

    Updated Things

---
+++ poke/MainVC.swift
@@ -0,0 +1,21 @@
+//
+//  MainVC.swift
+//  poke
+//
+//  Created by Hung Nguyen on 5/12/17.
+//  Copyright © 2017 Luvdub Nation. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
+
